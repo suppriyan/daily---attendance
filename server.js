641177f4c583e29c338227a09a26c1595a12ff8c
SUPPRIYAN K <suppriyanvishnu@gail.com>
2026-09-09 17:41:53 +0530

    fix homepage

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(express.static("public"));
 // ================= MYSQL CONNECTION =================
 
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQLHOST || process.env.MYSQL_HOST,
+    port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306,
+    user: process.env.MYSQLUSER || process.env.MYSQL_USER,
+    password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE
 });
 
 db.connect((err) => {
